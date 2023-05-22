9fb46d28588896083259b59e9d5b58ebf551438a
mesbahil <mesbahil@AG1115>
2023-05-22 14:49:23 +0330

    Added infrastructure classes to the domain project

---
+++ ShuttleServiceManagement.Domain/Errors/DomainErrors.cs
@@ -0,0 +1,20 @@
+﻿using ShuttleServiceManagement.Domain.Shared;
+
+namespace ShuttleServiceManagement.Domain.Errors
+{
+    public static class DomainErrors
+    {
+        public static class DriverName
+        {
+            public static readonly Error Empty = new("DriverName.Empty", "ورود نام راننده الزامی است.");
+            public static readonly Error MaxLength = new("DriverName.MaxLength", "تعداد کاراکترهای نام راننده بیش از حد مجاز است.");
+        }
+        public static class Bus
+        {
+            public static class Capacity
+            {
+                public static readonly Error Required = new("Bus.Capacity.Required", "ورود ظرفیت اتوبوس الزامی است.");
+            }
+        }
+    }
+}
---
+++ ShuttleServiceManagement.Domain/Infrastructure/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿namespace ShuttleServiceManagement.Domain.Infrastructure
+{
+    public abstract class BaseEntity : IEquatable<BaseEntity>
+    {
+        public Guid Id { get; set; }
+
+        protected BaseEntity(Guid id)
+        {
+            Id = id;
+        }
+        protected BaseEntity()
+        {
+
+        }
+
+        public bool Equals(BaseEntity? other)
+        {
+            return other != null && other.GetType() == GetType() && Id == other.Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj != null && obj is BaseEntity entity && Equals(entity);
+        }
+
+        public override int GetHashCode() => Id.GetHashCode() * 41;
+    }
+}
